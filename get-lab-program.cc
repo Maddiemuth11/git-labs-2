1cbd59139e294e6577bf38f8ce1c71a64e297fe3
Maddiemuth11 <157064106+Maddiemuth11@users.noreply.github.com>
2024-01-24 20:15:44 -0500

    Create get-lab-program.cc

---
+++ get-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Madeline Muth
+ *        Date: 01/24/23
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
